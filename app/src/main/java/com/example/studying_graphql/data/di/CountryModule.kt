6d9e8117383c8fc6ca4e70203111fcd7639b1e9c
EndrylCristian <endrylfiorotti.accenture@natura.net>
2023-07-14 14:05:05 -0300

    :hammer: Refactoring modules

--- app/src/main/java/com/example/studying_graphql/data/di/CountryModule.kt
+++ app/src/main/java/com/example/studying_graphql/data/di/CountryModule.kt
@@ -7,37 +7,28 @@ import com.example.studying_graphql.data.repository.CountryRepository
 import com.example.studying_graphql.data.useCase.GetCountriesUseCase
 import com.example.studying_graphql.data.useCase.GetCountryUseCase
 import com.example.studying_graphql.presenter.viewModel.CountryViewModel
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-private const val COUNTRY_INTERCEPTOR_KEY = "country_interceptor"
-private const val COUNTRY_GRAPHQL_KEY = "country_graphql"
-private const val COUNTRY_OKHTTP_KEY = "country_okhttp"
-
 val module = module {
-    factory<Interceptor>(named(COUNTRY_INTERCEPTOR_KEY)) { LoggingInterceptor() }
+    single { LoggingInterceptor() }
 
-    single(named(COUNTRY_OKHTTP_KEY)) {
+    single {
         OkHttpClient.Builder()
-            .addInterceptor(get<Interceptor>(named(COUNTRY_INTERCEPTOR_KEY)))
+            .addInterceptor(get<LoggingInterceptor>())
             .build()
     }
 
-    single(named(COUNTRY_GRAPHQL_KEY)) {
+    single {
         ApolloClient.Builder()
             .serverUrl("https://countries.trevorblades.com/graphql")
-            .okHttpClient(get(named(COUNTRY_OKHTTP_KEY)))
+            .okHttpClient(get())
             .build()
     }
 
-    factory { CountryRepository(get(named(COUNTRY_GRAPHQL_KEY))) }
-
+    factory { CountryRepository(get()) }
     factory { GetCountryUseCase(get()) }
-
     factory { GetCountriesUseCase(get()) }
-
     viewModel { CountryViewModel(get(), get()) }
 }
